ae2fdc6c55500ea04174fe6715de0e89a4dec6f8
Tai <ductai@gmail.com>
2023-12-13 13:45:37 +0700

    Create Student.cs in folder models

---
+++ btap git2/MODELS/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btap_git2.MODELS
+{
+    internal class Student
+    {
+    }
+}
